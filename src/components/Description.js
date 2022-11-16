14cbada53649aaf18444dbbe9e3e08a09b84e425
HungKNguyen <75971367+HungKNguyen@users.noreply.github.com>
2022-11-15 21:03:05 -0600

    Fix deploying issue

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,10 +4,10 @@ export function Description() {
     return (
         <Stack spacing={2}>
             <Typography variant="body2">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+
             </Typography>
             <Typography variant="caption" component="em">
-                Hung Nguyen, Riko Ogihara - Nov-17
+                Hung Nguyen, Riko Ogihara - Nov 17
             </Typography>
         </Stack>
     )
